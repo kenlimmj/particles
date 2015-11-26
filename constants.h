e8d4efbc4ef1c3ff9cf40ddcdf87b1c900dc8afe
ScottSWu <scott.wu.hjx@gmail.com>
2015-11-25 23:01:47 -0500

    Started translating code

--- constants.h
+++ constants.h
@@ -1,2 +1,26 @@
 // Constants go here
 
+const int ITERATIONS_PER_STEP = 10;
+
+const double GRAVITY = -9.807;
+
+const double PI = 3.14159265;
+
+const double PARTICLE_DENSITY = 6378.0 * 100.0;
+
+const double PARTICLE_MASS = 1.0;
+
+const double FORCE_EPSILON = 600.0;
+
+const double KERNEL_SIZE = 0.1;
+
+const double ARTIFICIAL_PRESSURE_STRENGTH = 1e-5;
+
+const double ARTIFICIAL_PRESSURE_RADIUS = 0.3 * KERNEL_SIZE;
+
+const double ARTIFICIAL_PRESSURE_POWER = 4;
+
+const double ARTIFICIAL_VISCOSITY = 1e-2;
+
+const double VORTICITY_COEFFICIENT = 1e-4;
+
